ef0ddd7b065ccbad7bcf1329e3371fe42090d128
David Spies <dnspies@gmail.com>
2024-06-16 11:04:00 -0700

    Unsafety

--- src/broadcast.rs
+++ src/broadcast.rs
@@ -1,15 +1,26 @@
-use std::sync::OnceLock;
+use std::{mem, sync::OnceLock};
 
+use consume_on_drop::{Consume, ConsumeOnDrop};
 use derive_where::derive_where;
 use parking_lot::Mutex;
 use tokio::sync::watch;
 
-use crate::{Arc, ArcPool};
+use crate::{Arc, ArcIndex, ArcPool};
 
 pub struct Sender<T> {
-    node_pool: ArcPool<Node<T>>,
-    next: Mutex<Arc<Node<T>>>,
+    inner: ConsumeOnDrop<SenderInner<T>>,
     notify: watch::Sender<()>,
+}
+
+struct SenderInner<T> {
+    node_pool: std::sync::Arc<ArcPool<Node<T>>>,
+    next: Mutex<Arc<Node<T>>>,
+}
+
+impl<T> Consume for SenderInner<T> {
+    fn consume(self) {
+        drop_with(self.next.into_inner(), &self.node_pool);
+    }
 }
 
 impl<T> Sender<T> {
@@ -17,17 +28,14 @@ impl<T> Sender<T> {
         if self.notify.is_closed() {
             return Err(value);
         }
-        let mut next = self.next.lock();
-        let next_arc = self.node_pool.alloc(Node::default());
-        next.set(value, next_arc.clone());
-        *next = next_arc;
+        self.inner.send(value);
         let _ = self.notify.send(());
         Ok(())
     }
 
     pub fn subscribe(&self) -> Receiver<T> {
         Receiver {
-            next: self.next.lock().clone(),
+            inner: ConsumeOnDrop::new(self.inner.subscribe()),
             notify: self.notify.subscribe(),
         }
     }
@@ -37,21 +45,34 @@ impl<T> Sender<T> {
     }
 }
 
-#[derive_where(Clone)]
-pub struct Receiver<T> {
-    next: Arc<Node<T>>,
-    notify: watch::Receiver<()>,
+impl<T> SenderInner<T> {
+    fn send(&self, value: T) {
+        let mut next = self.next.lock();
+        let next_arc = self.node_pool.alloc(Node::default());
+        next.set(value, next_arc.clone());
+        let prev_next = mem::replace(&mut *next, next_arc);
+        drop(next);
+        drop_with(prev_next, &self.node_pool);
+    }
+
+    fn subscribe(&self) -> ReceiverInner<T> {
+        ReceiverInner {
+            node_pool: self.node_pool.clone(),
+            next: self.next.lock().clone(),
+        }
+    }
 }
 
-pub enum TryRecvError {
-    Empty,
-    Disconnected,
+#[derive_where(Clone)]
+pub struct Receiver<T> {
+    inner: ConsumeOnDrop<ReceiverInner<T>>,
+    notify: watch::Receiver<()>,
 }
 
 impl<T: Clone> Receiver<T> {
     pub async fn recv(&mut self) -> Option<T> {
         loop {
-            if let Some(result) = self.try_next() {
+            if let Some(result) = self.inner.try_next() {
                 return Some(result);
             }
             self.notify.changed().await.ok()?;
@@ -59,7 +80,7 @@ impl<T: Clone> Receiver<T> {
     }
 
     pub fn try_recv(&mut self) -> Result<T, TryRecvError> {
-        self.try_next().ok_or_else(|| {
+        self.inner.try_next().ok_or_else(|| {
             if self.notify.has_changed().is_err() {
                 TryRecvError::Disconnected
             } else {
@@ -67,41 +88,84 @@ impl<T: Clone> Receiver<T> {
             }
         })
     }
+}
 
+#[derive_where(Clone)]
+struct ReceiverInner<T> {
+    node_pool: std::sync::Arc<ArcPool<Node<T>>>,
+    next: Arc<Node<T>>,
+}
+
+impl<T> Consume for ReceiverInner<T> {
+    fn consume(self) {
+        drop_with(self.next, &self.node_pool)
+    }
+}
+
+pub enum TryRecvError {
+    Empty,
+    Disconnected,
+}
+
+impl<T: Clone> ReceiverInner<T> {
     fn try_next(&mut self) -> Option<T> {
         let (value, next_node) = self.next.get()?;
         let value = value.clone();
-        self.next = next_node.clone();
+        let new_next = unsafe { Arc::clone_from_index(&self.node_pool, next_node) };
+        let old_next = mem::replace(&mut self.next, new_next);
+        drop_with(old_next, &self.node_pool);
         Some(value)
     }
 }
 
+fn drop_with<T>(mut node: Arc<Node<T>>, node_pool: &std::sync::Arc<ArcPool<Node<T>>>) {
+    loop {
+        let Some(raw_node) = Arc::into_inner(node) else {
+            return;
+        };
+        let Some((_, arc_index)) = raw_node.into_inner() else {
+            return;
+        };
+        node = unsafe { Arc::from_index(node_pool, arc_index) };
+    }
+}
+
 #[derive_where(Default)]
-struct Node<T>(OnceLock<(T, Arc<Node<T>>)>);
+struct Node<T>(OnceLock<(T, ArcIndex)>);
 
 impl<T> Node<T> {
     fn set(&self, value: T, next: Arc<Node<T>>) {
-        self.0.set((value, next)).unwrap_or_else(|_| {
-            panic!("Node::set: called twice on the same node");
-        });
+        self.0
+            .set((value, Arc::into_index(next)))
+            .unwrap_or_else(|_| {
+                panic!("Node::set: called twice on the same node");
+            });
     }
-    fn get(&self) -> Option<&(T, Arc<Node<T>>)> {
+    fn get(&self) -> Option<&(T, ArcIndex)> {
         self.0.get()
+    }
+    fn into_inner(self) -> Option<(T, ArcIndex)> {
+        self.0.into_inner()
     }
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let (tx, rx) = watch::channel(());
-    let node_pool = ArcPool::new();
+    let node_pool = std::sync::Arc::new(ArcPool::new());
     let initial = node_pool.alloc(Node::default());
     (
         Sender {
-            node_pool,
-            next: Mutex::new(initial.clone()),
+            inner: ConsumeOnDrop::new(SenderInner {
+                node_pool: node_pool.clone(),
+                next: Mutex::new(initial.clone()),
+            }),
             notify: tx,
         },
         Receiver {
-            next: initial,
+            inner: ConsumeOnDrop::new(ReceiverInner {
+                node_pool,
+                next: initial,
+            }),
             notify: rx,
         },
     )
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 use std::{
     cell::UnsafeCell,
     marker::PhantomData,
-    mem::MaybeUninit,
+    mem::{self, MaybeUninit},
     ops::Deref,
-    sync::atomic::{self, AtomicUsize},
+    sync::{
+        atomic::{self, AtomicUsize},
+        OnceLock,
+    },
 };
 
+use consume_on_drop::{Consume, ConsumeOnDrop};
 use parking_lot::{Mutex, RwLock, RwLockUpgradableReadGuard};
 
 pub mod broadcast;
@@ -28,7 +32,7 @@ impl<T> ArcPool<T> {
 
     pub fn with_capacity(cap: usize) -> Self {
         Self(RwLock::new(std::sync::Arc::new(
-            ArcPoolInner::with_capacity(cap),
+            ArcPoolInner::with_capacity(cap, 0, std::sync::Weak::new()),
         )))
     }
 
@@ -42,7 +46,13 @@ impl<T> ArcPool<T> {
         let read_guard = self.0.upgradable_read();
         let inner = std::sync::Arc::new(ArcPoolInner::with_capacity(
             (read_guard.mem.len() * 2).max(1),
+            read_guard.offset + read_guard.mem.len(),
+            std::sync::Arc::downgrade(&read_guard),
         ));
+        read_guard
+            .next
+            .set(inner.clone())
+            .unwrap_or_else(|_| unreachable!());
         let arc = inner.try_alloc(value).unwrap_or_else(|_| unreachable!());
         *RwLockUpgradableReadGuard::upgrade(read_guard) = inner;
         arc
@@ -57,6 +67,62 @@ pub struct Arc<T> {
 
 unsafe impl<T: Send + Sync> Send for Arc<T> {}
 unsafe impl<T: Send + Sync> Sync for Arc<T> {}
+
+pub struct ArcIndex(usize, ConsumeOnDrop<Panicker>);
+
+struct Panicker;
+
+impl Consume for Panicker {
+    fn consume(self) {
+        panic!("ArcIndex dropped without from_index")
+    }
+}
+
+impl<T> Arc<T> {
+    pub fn into_index(this: Self) -> ArcIndex {
+        unsafe { std::sync::Arc::increment_strong_count(&this.pool) }
+        ArcIndex(this.pool.offset + this.index, ConsumeOnDrop::new(Panicker))
+    }
+
+    pub fn into_inner(mut this: Self) -> Option<T> {
+        unsafe { this.drop_helper() }
+    }
+
+    unsafe fn drop_helper(&mut self) -> Option<T> {
+        let (ref_count, ptr) = &self.pool.mem[self.index];
+        if ref_count.fetch_sub(1, atomic::Ordering::Release) > 0 {
+            return None;
+        }
+        atomic::fence(atomic::Ordering::Acquire);
+        let ptr = ptr.get();
+        let result = mem::replace(&mut *ptr, MaybeUninit::uninit());
+        self.pool.free_list.lock().push(self.index);
+        Some(result.assume_init())
+    }
+
+    pub unsafe fn from_index(pool: &ArcPool<T>, index: ArcIndex) -> Self {
+        let result = Self::clone_from_index(pool, &index);
+        std::sync::Arc::decrement_strong_count(&result.pool);
+        let ArcIndex(_, panicker) = index;
+        let _ = ConsumeOnDrop::into_inner(panicker);
+        result
+    }
+
+    pub unsafe fn clone_from_index(
+        ArcPool(pool): &ArcPool<T>,
+        &ArcIndex(index, _): &ArcIndex,
+    ) -> Self {
+        let mut pool = pool.read().clone();
+        while index < pool.offset {
+            pool = pool.prev.upgrade().unwrap();
+        }
+        Self {
+            index: index - pool.offset,
+            pool,
+            _phantom: PhantomData,
+        }
+    }
+}
 
 impl<T> Clone for Arc<T> {
     fn clone(&self) -> Self {
@@ -82,29 +148,28 @@ impl<T> Deref for Arc<T> {
 
 impl<T> Drop for Arc<T> {
     fn drop(&mut self) {
-        let (ref_count, ptr) = &self.pool.mem[self.index];
-        if ref_count.fetch_sub(1, atomic::Ordering::Release) > 0 {
-            return;
-        }
-        atomic::fence(atomic::Ordering::Acquire);
-        let ptr = ptr.get();
-        unsafe { (*ptr).assume_init_drop() };
-        self.pool.free_list.lock().push(self.index);
+        drop(unsafe { self.drop_helper() })
     }
 }
 
 struct ArcPoolInner<T> {
     free_list: Mutex<Vec<usize>>,
     mem: Box<[(AtomicUsize, UnsafeCell<MaybeUninit<T>>)]>,
+    offset: usize,
+    prev: std::sync::Weak<ArcPoolInner<T>>,
+    next: OnceLock<std::sync::Arc<ArcPoolInner<T>>>,
 }
 
 impl<T> ArcPoolInner<T> {
-    fn with_capacity(cap: usize) -> Self {
+    fn with_capacity(cap: usize, offset: usize, prev: std::sync::Weak<ArcPoolInner<T>>) -> Self {
         Self {
             free_list: Mutex::new((0..cap).rev().collect()),
             mem: (0..cap)
                 .map(|_| (AtomicUsize::new(0), UnsafeCell::new(MaybeUninit::uninit())))
                 .collect(),
+            offset,
+            prev,
+            next: OnceLock::new(),
         }
     }
 
