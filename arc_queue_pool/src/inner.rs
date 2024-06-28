17d5d4995d1dc8bf849912f8d59c2db4ffe2ed86
David Spies <dnspies@gmail.com>
2024-06-28 11:24:31 -0700

    ArcData

--- arc_queue_pool/src/inner.rs
+++ arc_queue_pool/src/inner.rs
@@ -55,9 +55,11 @@ impl<T> ArcPool<T> {
         };
         ArcInner {
             pool: self.clone(),
-            ptr: &queue[inner_index],
-            outer_index,
-            inner_index,
+            data: ArcData {
+                ptr: &queue[inner_index],
+                outer_index,
+                inner_index,
+            },
         }
     }
 
@@ -83,9 +85,11 @@ impl<T> ArcPool<T> {
             value,
             Some(ArcInner {
                 pool: self,
-                ptr,
-                outer_index,
-                inner_index,
+                data: ArcData {
+                    ptr,
+                    outer_index,
+                    inner_index,
+                },
             }),
         )
     }
@@ -93,6 +97,10 @@ impl<T> ArcPool<T> {
 
 pub(crate) struct ArcInner<T> {
     pool: ArcPool<T>,
+    data: ArcData<T>,
+}
+
+struct ArcData<T> {
     ptr: *const Entry<T>,
     outer_index: StableIndex,
     inner_index: StableIndex,
@@ -103,14 +111,14 @@ impl<T> ArcInner<T> {
         let guard = self.pool.0.read();
         let new_outer_index;
         let new_ptr;
-        let mut new_inner_index = self.inner_index.next_idx();
-        match guard[self.outer_index].get(new_inner_index) {
+        let mut new_inner_index = self.data.inner_index.next_idx();
+        match guard[self.data.outer_index].get(new_inner_index) {
             Some(ptr) => {
-                new_outer_index = self.outer_index;
+                new_outer_index = self.data.outer_index;
                 new_ptr = ptr;
             }
             None => {
-                new_outer_index = self.outer_index.next_idx();
+                new_outer_index = self.data.outer_index.next_idx();
                 let next_queue = guard.get(new_outer_index)?;
                 new_inner_index = next_queue.front_idx().unwrap();
                 new_ptr = next_queue.front().unwrap();
@@ -119,14 +127,16 @@ impl<T> ArcInner<T> {
         new_ptr.refcount.fetch_add(1, atomic::Ordering::Relaxed);
         Some(ArcInner {
             pool: self.pool.clone(),
-            ptr: new_ptr,
-            outer_index: new_outer_index,
-            inner_index: new_inner_index,
+            data: ArcData {
+                ptr: new_ptr,
+                outer_index: new_outer_index,
+                inner_index: new_inner_index,
+            },
         })
     }
 
     pub(crate) fn into_inner_and_next(self) -> Option<(T, Option<ArcInner<T>>)> {
-        let Entry { refcount, value: _ } = unsafe { &*self.ptr };
+        let Entry { refcount, value: _ } = unsafe { &*self.data.ptr };
         let mut cur_count = refcount.load(atomic::Ordering::Relaxed);
         while cur_count > 1 {
             match refcount.compare_exchange_weak(
@@ -144,7 +154,7 @@ impl<T> ArcInner<T> {
             .compare_exchange(1, 0, atomic::Ordering::Acquire, atomic::Ordering::Relaxed)
             .unwrap();
         let front_ptr = guard.front().unwrap().front().unwrap();
-        if !ptr::addr_eq(front_ptr, self.ptr) {
+        if !ptr::addr_eq(front_ptr, self.data.ptr) {
             return None;
         }
         drop(guard);
@@ -152,14 +162,14 @@ impl<T> ArcInner<T> {
     }
 
     pub(crate) fn try_unwrap_and_next(self) -> Result<(T, Option<ArcInner<T>>), ArcInner<T>> {
-        let Entry { refcount, value: _ } = unsafe { &*self.ptr };
+        let Entry { refcount, value: _ } = unsafe { &*self.data.ptr };
         let cur_count = refcount.load(atomic::Ordering::Relaxed);
         if cur_count > 1 {
             return Err(self);
         }
         let guard = self.pool.0.read();
         let front_ptr = guard.front().unwrap().front().unwrap();
-        if !ptr::addr_eq(front_ptr, self.ptr) {
+        if !ptr::addr_eq(front_ptr, self.data.ptr) {
             drop(guard);
             return Err(self);
         }
@@ -171,11 +181,11 @@ impl<T> ArcInner<T> {
     }
 
     pub(crate) fn get(&self) -> &T {
-        &unsafe { &*self.ptr }.value
+        &unsafe { &*self.data.ptr }.value
     }
 
     pub(crate) fn ref_count(&self) -> usize {
-        unsafe { &*self.ptr }
+        unsafe { &*self.data.ptr }
             .refcount
             .load(atomic::Ordering::Relaxed)
     }
@@ -185,25 +195,30 @@ impl<T> Clone for ArcInner<T> {
     fn clone(&self) -> Self {
         let &Self {
             ref pool,
-            ptr,
-            outer_index,
-            inner_index,
+            data:
+                ArcData {
+                    ptr,
+                    outer_index,
+                    inner_index,
+                },
         } = self;
         unsafe { &*ptr }
             .refcount
             .fetch_add(1, atomic::Ordering::Relaxed);
         ArcInner {
             pool: pool.clone(),
-            ptr,
-            outer_index,
-            inner_index,
+            data: ArcData {
+                ptr,
+                outer_index,
+                inner_index,
+            },
         }
     }
 }
 
 impl<T> Consume for ArcInner<T> {
     fn consume(self) {
-        let old_count = unsafe { &*self.ptr }
+        let old_count = unsafe { &*self.data.ptr }
             .refcount
             .fetch_sub(1, atomic::Ordering::Release);
         if old_count > 1 {
@@ -211,7 +226,7 @@ impl<T> Consume for ArcInner<T> {
         }
         let guard = self.pool.0.read();
         let front_ptr = guard.front().unwrap().front();
-        if !front_ptr.is_some_and(|ptr| ptr::addr_eq(ptr, self.ptr)) {
+        if !front_ptr.is_some_and(|ptr| ptr::addr_eq(ptr, self.data.ptr)) {
             return;
         }
         drop(guard);
